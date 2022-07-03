5f8240632b66723633f34ffe6d995beb772dc22f
Gökmen Görgen <gkmngrgn@gmail.com>
2022-07-03 22:30:01 +0300

    create client and server.

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,38 @@
-use std::net::TcpListener;
+use std::{
+    net::{Shutdown, TcpListener, TcpStream},
+    thread,
+};
 
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream(&mut data) {
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        }
+        Err(_) => {
+            eprintln!(
+                "An error occured, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
 
-fn main() {
-
+fn run_server() {
+    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || handle_client(stream));
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+    drop(listener);
 }
